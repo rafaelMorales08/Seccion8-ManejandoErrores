30a82d76459e40da018093503b9b46573f352fde
rmorales <rmorales@MTY-LP-RMORALES>
2022-12-29 17:23:20 -0600

    Agregar archivos de proyecto.

---
+++ ManejandoErrores/EscritorPersonalizado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejandoErrores
+{
+    internal class EscritorPersonalizado : TextWriter
+    {
+        public override Encoding Encoding => throw new NotImplementedException();
+        public override void WriteLine(string? value)
+        {
+            base.WriteLine($"{DateTime.Now.ToString("dddd")} {value}");
+        }
+    }
+}
---
+++ ManejandoErrores/Program.cs
@@ -0,0 +1,48 @@
+﻿//debug writeLine de apunte
+using ManejandoErrores;
+using System.Diagnostics;
+
+var escritorConsola = new TextWriterTraceListener(System.Console.Out);
+Trace.Listeners.Add(escritorConsola);
+
+Console.WriteLine("esta linea se ha ejucutado");
+Debug.WriteLine("Este mensaje solo aparecen en modo debug");
+
+var escritorPeronalizaod = new TextWriterTraceListener(new EscritorPersonalizado());
+Trace.Listeners.Add(escritorPeronalizaod);
+Trace.Listeners.Add(escritorConsola);
+
+//tracePoint
+
+
+//excepciones
+
+Console.WriteLine("antes del error");
+
+throw new Exception();
+
+Console.WriteLine("despues del error");
+
+
+
+try
+{
+    Console.WriteLine("antes del error");
+
+    throw new Exception();
+
+    Console.WriteLine("despues del error");
+
+
+}
+catch (NotImplementedException ex)
+{
+    Console.WriteLine("hA OCURRIDO UNA EXCEPCION ");
+    Console.WriteLine(ex.Message );
+	throw;
+}
+finally
+{
+
+    Console.WriteLine("bloque fanaly");
+}
